d70b2e7082490f42de32c3eb36e8f3bff0f1b36e
Simon Tabor <me@simontabor.com>
2016-02-28 00:12:12 +0000

    update about

--- serenity.js
+++ serenity.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ignore: [ '/_site', '/.git', '/README.md' ],
+  ignore: [ '/_site', '/.git', '/README.md', '/node_modules', '/bin' ],
   asset_paths: true,
   autoprefixer: ["last 4 versions", "> 1%", "Firefox ESR", "Opera 12.1", "Explorer 8"]
 };
